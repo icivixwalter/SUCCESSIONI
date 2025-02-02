4f95f32ed87027a70601c60231a3ca129c96e1fe
walter <icivixwalter@gmail.com>
2025-02-02 22:20:04 +0100

    inizio successioni

---
+++ git_OpenGitHub.bat
@@ -0,0 +1,31 @@
+
+
+start https://github.com/icivixwalter/MENU.git
+
+
+
+
+
+
+
+
+
+
+
+goto utilita
+
+
+PATH_APRI_ERRORI_GIT
+	Path e file per la gestione degli errori di git che si trova nel file omonino.
+
+	@errori@git_(aprire il file con start per gestire gli errori di git)
+		Start "apri il file" C:\CASA\LINGUAGGI\GIT_DESKTOP\Errori_Git\Errori_Git.md
+
+	PATH_TUTORIAL_GIT
+		Per aprire il file tutorial di git per i vari comandi
+			@comandi@github_(si trovano in questo file)
+				
+				Start "apri il file" C:\CASA\LINGUAGGI\GIT_DESKTOP\Errori_Git\Errori_Git.md
+
+
+:utilita
